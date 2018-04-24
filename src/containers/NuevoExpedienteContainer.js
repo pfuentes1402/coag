8d83e11af5619dbbb55a8ad8cfb00a8d503095ed
alexrma <alexrma@gmail.com>
2018-04-24 14:02:15 +0200

    Transformando redux-form  a component, falla en las validaciones

--- src/containers/NuevoExpedienteContainer.js
+++ src/containers/NuevoExpedienteContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import NuevoExpediente from '../components/NewExpedient/NewExpedient';
+import NuevoExpediente2 from '../components/NewExpedient/NewExpedient2';
 import { withStyles } from 'material-ui/styles';
 import {Grid} from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
@@ -13,7 +14,8 @@ class NuevoExpedienteContainer extends Component {
     render() {
         return (
             <div className="nuevoExpedienteContainer">               
-               <NuevoExpediente/>                
+             
+               <NuevoExpediente2/>                                
             </div>
         );
     }
