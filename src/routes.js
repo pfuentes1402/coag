ce55d72069530b71a649d352912b78ee3858fa23
yago.benitez <yago.hacce@gmail.com>
2018-05-10 13:10:49 +0200

    selector expediente

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import HomeContainer from './containers/HomeContainer';
 import NuevoExpedienteContainer from './containers/NuevoExpedienteContainer';
+import SelectorTipoExpedienteContainer from './containers/SelectorTipoExpedienteContainer';
 import MainContainer from './containers/index';
 
 export default (
@@ -10,5 +11,6 @@ export default (
       <Route component={MainContainer}/>
       <Route exact path='/' component={HomeContainer}/>
       <Route path='/nuevo-expediente' component={NuevoExpedienteContainer}/>
+      <Route path='/selectorExpediente' component={SelectorTipoExpedienteContainer}/>
   </div>
 );
