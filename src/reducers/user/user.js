b8683ec13c63f62b1010825650a70e5b77f0e6c5
yago.benitez <yago.hacce@gmail.com>
2018-05-28 19:00:34 +0200

    head

--- src/reducers/user/user.js
+++ src/reducers/user/user.js
@@ -1,10 +1,7 @@
 const initialState = { name: "userName", config: "useConfig" };
 
 const reducer = (state = { user: initialState }, action) => {
-<<<<<<< HEAD
-=======
   
->>>>>>> c7837c4e597b742bceab441814823923ef4a286c
   switch (action.type) {
     default:
       return state;
