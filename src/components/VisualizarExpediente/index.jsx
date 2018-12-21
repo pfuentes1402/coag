ab3c4e8ddd5f7aca080257a8df5f37908fcaa2ee
pedrofuentespi@gmail.com <pedrofuentespi@gmail.com>
2018-12-21 17:14:35 -0500

    crenado el componente de visualizar el expediente que se obtiene

---
+++ src/components/VisualizarExpediente/index.jsx
@@ -0,0 +1,11 @@
+import React ,{Component} from 'react';
+
+class VisualizarExpediente extends Component{
+  render(){
+    return(
+      <div>En proceso de inicialización de desarrollo</div>
+    )
+  }
+}
+
+export default VisualizarExpediente;
