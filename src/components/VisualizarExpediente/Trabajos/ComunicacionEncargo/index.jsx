77547f05bd08ac7dec725b906b138de1e4dd6371
pedrofuentespi@gmail.com <pedrofuentespi@gmail.com>
2019-01-15 11:58:48 -0500

    Incorporando el componente Tipo de Expediente al comp principal

--- src/components/VisualizarExpediente/Trabajos/ComunicacionEncargo/index.jsx
+++ src/components/VisualizarExpediente/Trabajos/ComunicacionEncargo/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid } from '@material-ui/core';
 import FichaExpediente from './fichaExpediente';
 import ListaTrabajos from './listaTrabajos';
+import TipoExpediente from './tipoExpediente';
 
 class TrabajoComunicacion extends Component {
   constructor(props) {
@@ -13,6 +14,8 @@ class TrabajoComunicacion extends Component {
       <Grid container spacing={0}>
         <Grid item md={6} xs={12} >
           <FichaExpediente expediente={this.props.expediente} sourceExpediente={this.props.expediente.Expediente.length > 0 ? this.props.expediente.Expediente[0] : {}}/>
+
+          <TipoExpediente expediente={this.props.expediente} sourceExpediente={this.props.expediente.Expediente.length > 0 ? this.props.expediente.Expediente[0] : {}}/>
         </Grid>
         <Grid item md={6} xs={12} >
           <ListaTrabajos expediente={this.props.expediente}/>
