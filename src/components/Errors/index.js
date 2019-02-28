3df164db73f9696e4c6ed01aee794a4ec822f47d
Dayni <dayni88@gmail.com>
2019-02-28 10:34:17 -0500

    estilo de los mensajes

--- src/components/Errors/index.js
+++ src/components/Errors/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import { withLocalize } from "react-localize-redux";
 import { withStyles } from '@material-ui/core/styles';
+import { Translate } from "react-localize-redux";
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import { connect } from "react-redux";
 import {fetchErrorExpediente} from "../../actions/expedientes";
 import {fetchErrorTrabajo} from "../../actions/trabajos";
-
+import {Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, Button} from "@material-ui/core";
+import ReactQuill from "react-quill";
 
 const mapStateToProps = (state) => (
     {
@@ -44,35 +46,30 @@ class ErrorSnackbars extends React.Component {
         const { classes } = this.props;
         return (
             <div>
-
-                <Snackbar
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'center',
-                    }}
+                <Dialog
                     open={this.props.errorExpediente.length >0 || this.props.errorTrabajo.length >0}
-                    autoHideDuration={5000}
                     onClose={this.handleClose}
-                    ContentProps={{
-                        'aria-describedby': 'message-id',
-                    }}
-                    message={<span id="message-id">{this.props.errorExpediente.length > 0 ? this.props.errorExpediente[0].Mensaje : (this.props.errorTrabajo.length >0 ? this.props.errorTrabajo[0].Mensaje: "")}</span>}
-                    action={[
-                        <IconButton
-                            key="close"
-                            aria-label="Close"
-                            color="inherit"
-                            className={classes.close}
-                            onClick={this.handleClose}
-                        >
-                            <CloseIcon />
-                        </IconButton>,
-                    ]}
-                />
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        <Translate id="languages.generalButton.mensaje"/>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            <ReactQuill value={this.props.errorExpediente.length > 0 ? this.props.errorExpediente[0].Mensaje : (this.props.errorTrabajo.length >0 ? this.props.errorTrabajo[0].Mensaje: "")} readOnly theme='bubble' />
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary" autoFocus>
+                            <Translate id="languages.generalButton.aceptar"/>
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(ErrorSnackbars));
+export default connect(mapStateToProps, mapDispatchToProps)(withLocalize(withStyles(styles)(ErrorSnackbars)));
